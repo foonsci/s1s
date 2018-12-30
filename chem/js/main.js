af213658b7ee8923337a6fcb0bc9ffaa7424af89
NGOLDWEEKLY <ngoldweekly@gmail.com>
2018-12-30 19:45:04 +0800

    fd

--- chem/js/main.js
+++ chem/js/main.js
@@ -69,7 +69,79 @@ function qnxt(arr){
 				 
 				 
 				 
-				 
+	
+	
+	
+			
+			 function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      testAPI();
+    } else {
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+
+      appId      : '731763497011360',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.2'
+    });
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+	    			    arr= response.name;
+	res = res + "&q0=" + arr;
+        setCookie("q0", arr, 30);
+        $('.Qintro').hide();
+        $('.Qlists').show();
+        $('#1').show();
+		$("span[contenteditable]").focus();
+    });
+  }
+			
+			
+			
+			
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
     function reschk() {
         
         $(".Qloader").show();
